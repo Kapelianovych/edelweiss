a6b0c50d5655bbda9a44b0b3c100905fd89d38a7
Kapelianovych Yevhen <kapelianovych.y.v@gmail.com>
2021-10-09 10:16:31 +0300

    fix: ignore build directories while executing tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,9 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'jsdom',
+	modulePathIgnorePatterns: [
+		'<rootDir>/packages/edelweiss/build/',
+		'<rootDir>/packages/edelweiss-cli/build/',
+		'<rootDir>/packages/edelweiss-ssr/build/',
+	],
 };
